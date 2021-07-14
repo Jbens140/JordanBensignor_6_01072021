efaf80742ab5abc98ffce75d87eba748967a96db
Jbens140 <75929738+Jbens140@users.noreply.github.com>
2021-07-14 17:56:16 +0200

    ajout de plugin pour les differents models + ajout commentaires

--- backend/models/Sauces.js
+++ backend/models/Sauces.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
+const sanitizerPlugin = require('mongoose-sanitizer-plugin');
+
+// Création d'un schema mangoose et L'id est généré automatiquement par MongoDB
 
 const sauceSchema = mongoose.Schema({
-    
-    id: {type: String, required: true },
-    userId: {type: String, required: true },
-    name: {type: String, required: true },
-    manufacturer: {type: String, required: true },
-    description: {type: String, required: true },
-    mainPepper: {type: String, required: true },
-    imageUrl: {type: String, required: true },
-    heat: {type: Number, required: true },
-    likes: {type: Number, required: true },
-    dislikes: {type: Number, required: true },
-    usersLiked: {type: [String], required: true },
-    usersDisliked: {type: [String], required: true },
+
+    // UserId du createur
+    userId: { type: String, required: true },
+    // Nom de la sauce
+    name: { type: String, required: true },
+    // Créateur de la sauce
+    manufacturer: { type: String, required: true },
+    // description de la sauce
+    description: { type: String, required: true },
+    // Ingredients qui pimentent la sauce
+    mainPepper: { type: String, required: true },
+    // Adresse de l'image de presentation de la sauce
+    imageUrl: { type: String, required: true },
+    // Niveau de piquant de la sauce
+    heat: { type: Number, required: true },
+    // nombre de Like reçu
+    likes: { type: Number },
+    // nombre de dislike reçu
+    dislikes: { type: Number },
+    // Utilisateurs qui Like la sauce
+    usersLiked: { type: [String] },
+    // Utilisateur qui DisLike la sauce
+    usersDisliked: { type: [String] },
+
 
 });
+
+sauceSchema.plugin(sanitizerPlugin);
 
 module.exports = mongoose.model('Sauce', sauceSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,33 @@
+
+// Création d'un model user avec mongoose, on importe donc mongoose
 const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+const uniqueValidator = require('mongoose-unique-validator');
+const sanitizerPlugin = require('mongoose-sanitizer-plugin');
 
 const userSchema = mongoose.Schema({
-    userId: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+
+    // L'email doit être unique
+
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Veuillez entrer votre adresse email"],
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]
+    },
+
+    // enregistrement du mot de pass 
+    password: {
+        type: String,
+        required: [true, "Veuillez choisir un mot de passe"]
+    }
 });
 
+// Plugin pour garantir un email unique
+userSchema.plugin(uniqueValidator);
+// Plugin pour Mongoose qui purifie les champs du model avant de les enregistrer dans la base MongoDB.
+userSchema.plugin(sanitizerPlugin);
+
+// On exporte ce schéma sous forme de modèle : le modèle s'appellera user et on lui passe le shéma de données
 module.exports = mongoose.model('User', userSchema);
